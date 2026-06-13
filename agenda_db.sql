3aded50977f25dd3f365e29f38545eea67fdccf2
Humbert001 <hum_bertoaugust0@outlook.com>
2026-06-13 16:29:23 -0300

    Create agenda_db.sql
    Criando o agenda_db.sql Projeto Integrador

---
+++ agenda_db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS agenda_db;
+USE agenda_db;
+
+CREATE TABLE IF NOT EXISTS contatos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
